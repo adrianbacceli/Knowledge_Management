d9756a92b140d709a5943cd0df8579c9ae5d1cdd
adrianbacceli <47468730+adrianbacceli@users.noreply.github.com>
2025-05-09 23:24:05 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Adrian Knowledge",
+    pageTitle: "Adrian's KB",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
